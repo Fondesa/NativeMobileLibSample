abc5e90aeb51c5e9d5784dfd02c2b7b276b08ad6
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-07-01 22:52:32 +0200

    Change cmake for XCode

--- xcode_build/native_mobile_lib_sample.build/Debug/ZERO_CHECK.build/Script-F2154161A3A14A7BB109AED8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/antoniolig/Developer/c++/native_mobile_lib/xcode_build -f /Users/antoniolig/Developer/c++/native_mobile_lib/xcode_build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
