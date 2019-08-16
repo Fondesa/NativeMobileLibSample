6cb0c096c8fba70ce6a1fe1513c13831977316c0
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-08-17 00:35:59 +0200

    Add tests for Db::Client

--- lib/test/database/database_client_test.cpp
+++ lib/test/database/database_client_test.cpp
@@ -1,8 +1,38 @@
-#include "gtest/gtest.h"
+#include "database_exception.hpp"
+#include "database/sqlite_database.hpp"
+#include "database_client_test.hpp"
 #include "database_client.hpp"
 #include "note/drafts_repository.hpp"
 
-TEST(DatabaseClientTest, whenGetIsInvokedBeforeCreateThenExceptionIsThrown) {
-    Db::Client::get();
-    ASSERT_TRUE(true);
+void DatabaseClientTest::TearDown() {
+    Db::Client::release();
+}
+
+TEST_F(DatabaseClientTest, givenUninitializedDbWhenGetIsInvokedThenExceptionIsThrown) {
+    EXPECT_THROW(Db::Client::get(), Db::Exception);
+}
+
+TEST_F(DatabaseClientTest, givenReleasedDbWhenGetIsInvokedThenExceptionIsThrown) {
+    Db::Client::create(":memory:");
+    Db::Client::release();
+
+    EXPECT_THROW(Db::Client::get(), Db::Exception);
+}
+
+TEST_F(DatabaseClientTest, givenInitializedDbWhenGetIsInvokedThenDbIsReturned) {
+    Db::Client::create(":memory:");
+    auto db = Db::Client::get();
+
+    ASSERT_TRUE(db != nullptr);
+    EXPECT_TRUE(std::dynamic_pointer_cast<Db::Sql::Database>(db));
+}
+
+TEST_F(DatabaseClientTest, givenInitializedDbWhenInitializeIsInvokedMultipleTimesThenDbIsNotCreatedAgain) {
+    Db::Client::create(":memory:");
+    auto db = Db::Client::get();
+    Db::Client::create(":memory:");
+    auto secondDb = Db::Client::get();
+
+    // They should point to the same location.
+    EXPECT_EQ(secondDb, db);
 }
