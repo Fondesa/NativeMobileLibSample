7069055d6dab0304eb6e39fe3420494b4d5343eb
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-08-27 01:10:18 +0200

    Update run tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,6 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 projectDir=${scriptDir}
 testsBuildDir=${projectDir}/build/tests
 
-echo "Building all the tests and their dependencies..."
 cmake -B${testsBuildDir} \
     -DBUILD_TESTS=ON
-(cd ${testsBuildDir} && make)
-echo "Executing the lib tests..."
-${testsBuildDir}/out/lib_tests
+(cd ${testsBuildDir} && make run-tests)
