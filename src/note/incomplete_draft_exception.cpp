f612acac6a4c2d8ad9b1f35af9bbdc905ec8fbe5
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-09-28 20:47:21 +0200

    Add check on complete draft inside IncompleteDraftException body

--- src/note/incomplete_draft_exception.cpp
+++ src/note/incomplete_draft_exception.cpp
@@ -3,6 +3,10 @@
 IncompleteDraftException::IncompleteDraftException(const MutableDraft &draft) {
     auto hasTitle = draft.hasTitle();
     auto hasDescription = draft.hasDescription();
+    if (hasTitle && hasDescription) {
+        this->msg = "The new draft is complete so this exception mustn't be thrown, use another exception.";
+        return;
+    }
     this->msg = "The following fields of the new draft aren't set: ";
     if (!hasTitle) {
         msg += "title";
@@ -18,6 +22,12 @@ IncompleteDraftException::IncompleteDraftException(const MutableDraft &draft) {
 IncompleteDraftException::IncompleteDraftException(int id, const MutableDraft &draft) {
     auto hasTitle = draft.hasTitle();
     auto hasDescription = draft.hasDescription();
+    if (hasTitle && hasDescription) {
+        this->msg = "The draft with id " +
+            std::to_string(id) +
+            " is complete so this exception mustn't be thrown, use another exception.";
+        return;
+    }
     this->msg = "The following fields of the existing draft with id " +
         std::to_string(id) +
         " aren't set: ";
