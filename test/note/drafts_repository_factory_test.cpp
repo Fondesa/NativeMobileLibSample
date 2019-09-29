1998b2c23745c07017b03e7d165c925573047068
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-09-29 12:38:21 +0200

    Add tests to repository factories

---
+++ test/note/drafts_repository_factory_test.cpp
@@ -0,0 +1,19 @@
+#include "database_client.hpp"
+#include "drafts_repository_factory_test.hpp"
+#include "note/drafts_repository_factory.hpp"
+#include "note/drafts_repository_impl.hpp"
+
+void DraftsRepositoryFactoryTest::SetUp() {
+    Db::Client::create(":memory:");
+}
+
+void DraftsRepositoryFactoryTest::TearDown() {
+    Db::Client::release();
+}
+
+TEST_F(DraftsRepositoryFactoryTest, whenCreateIsInvokedThenDraftsRepositoryIsCreated) {
+    auto repository = DraftsRepositoryFactory::create();
+
+    ASSERT_TRUE(repository != nullptr);
+    EXPECT_TRUE(std::dynamic_pointer_cast<DraftsRepositoryImpl>(repository));
+}
