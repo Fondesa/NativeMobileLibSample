dc4ddf4a15f6a090f203f21c610d0cd98fbe2b49
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-10-15 20:59:03 +0200

    Add linux support

--- .ci/before-install.sh
+++ .ci/before-install.sh
@@ -5,7 +5,15 @@ brew update
 # The Android NDK toolchain file is needed to build the library for Android so, before building, Travis will download
 # the NDK R20, unzip them and export their path to use it inside the build script.
 if [[ "$BUILD_ANDROID" == "true" ]]; then
-    curl https://dl.google.com/android/repository/android-ndk-r20-darwin-x86_64.zip -o ndk-r20.zip
-    unzip -q ndk-r20.zip
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        curl https://dl.google.com/android/repository/android-ndk-r20-darwin-x86_64.zip -o ndk-r20.zip
+        unzip -q ndk-r20.zip
+    elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        wget -P ndk-r20.zip https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
+        unzip -q ndk-r20.zip
+    else
+        echo "The OS $TRAVIS_OS_NAME isn't supported."
+        exit 1
+    fi
     export ANDROID_NDK=android-ndk-r20
 fi
