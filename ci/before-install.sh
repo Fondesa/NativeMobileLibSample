dee3845dd131a9d47a064382d77518716abfd08b
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-10-15 21:58:03 +0200

    Update install

--- ci/before-install.sh
+++ ci/before-install.sh
@@ -11,8 +11,8 @@ if [[ "$BUILD_ANDROID" == "true" ]]; then
         curl https://dl.google.com/android/repository/android-ndk-r20-darwin-x86_64.zip -o ndk-r20.zip
         unzip -q ndk-r20.zip
     elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        wget -P ndk-r20.zip https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
-        unzip -q ndk-r20.zip
+        wget https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
+        unzip -q android-ndk-r20-linux-x86_64.zip
     else
         echo "The OS $TRAVIS_OS_NAME isn't supported."
         exit 1
--- ci/install.sh
+++ ci/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-formulae=()
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+function install_osx() {
+    local formulae=()
     # Install GCC if needed.
     if [[ "$CC" == "$CC_GCC" ]]; then
         formulae+=(gcc)
@@ -13,47 +12,69 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     if [[ "$RUN_TESTS_WITH_COVERAGE" == "true" ]]; then
         formulae+=(lcov)
     fi
+
+    for formula in "${formulae[@]}"; do
+        for retry in $(seq 1 3); do
+            echo "Upgrading ${formula}"
+            brew outdated | grep -q "$formula"
+            STATUS="$?"
+            if [ "${STATUS}" -ne 0 ]; then
+                echo "Already up to date or not installed."
+                break
+            fi
+
+            brew upgrade "$formula"
+            STATUS="$?"
+            if [ "${STATUS}" -eq 0 ]; then
+                break
+            fi
+            echo "Attempt ${retry} failed."
+            if [ "${retry}" -eq 3 ]; then
+                echo "Too many retries. Aborting."
+                exit 1
+            else
+                echo "Retrying..."
+            fi
+        done
+    done
+
+    for formula in "${formulae[@]}"; do
+        for retry in $(seq 1 3); do
+            echo "Installing ${formula}"
+            brew list | grep -q "$formula" || brew install "$formula"
+            STATUS="$?"
+            if [ "${STATUS}" -eq 0 ]; then
+                break
+            fi
+            echo "Attempt ${retry} failed."
+            if [ "${retry}" -eq 3 ]; then
+                echo "Too many retries. Aborting."
+                exit 1
+            else
+                echo "Retrying..."
+            fi
+        done
+    done
+}
+
+function install_linux() {
+    sudo add-apt-repository ppa:jonathonf/gcc-9.2
+    sudo apt-get update
+
+    # Install GCC if needed.
+    if [[ "$CC" == "$CC_GCC" ]]; then
+        sudo apt-get install gcc-9
+    fi
+
+    # Install LCOV only when the tests are run with coverage.
+    # This is optimal since we don't want to install it in jobs which don't use it.
+    if [[ "$RUN_TESTS_WITH_COVERAGE" == "true" ]]; then
+        sudo apt-get install lcov
+    fi
+}
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    install_osx
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    install_linux
 fi
-
-for formula in "${formulae[@]}"; do
-    for retry in $(seq 1 3); do
-        echo "Upgrading ${formula}"
-        brew outdated | grep -q "$formula"
-        STATUS="$?"
-        if [ "${STATUS}" -ne 0 ]; then
-            echo "Already up to date or not installed."
-            break
-        fi
-
-        brew upgrade "$formula"
-        STATUS="$?"
-        if [ "${STATUS}" -eq 0 ]; then
-            break
-        fi
-        echo "Attempt ${retry} failed."
-        if [ "${retry}" -eq 3 ]; then
-            echo "Too many retries. Aborting."
-            exit 1
-        else
-            echo "Retrying..."
-        fi
-    done
-done
-
-for formula in "${formulae[@]}"; do
-    for retry in $(seq 1 3); do
-        echo "Installing ${formula}"
-        brew list | grep -q "$formula" || brew install "$formula"
-        STATUS="$?"
-        if [ "${STATUS}" -eq 0 ]; then
-            break
-        fi
-        echo "Attempt ${retry} failed."
-        if [ "${retry}" -eq 3 ]; then
-            echo "Too many retries. Aborting."
-            exit 1
-        else
-            echo "Retrying..."
-        fi
-    done
-done
