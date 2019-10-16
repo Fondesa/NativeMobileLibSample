4f68233c83a6153f04ff0a77f1cfbded135a4cd0
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-10-16 02:30:53 +0200

    Fix install

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,7 @@
 function install_osx() {
     local formulae=()
     if [[ "$CC" == "$CC_GCC_OSX" ]]; then
-        # Install the last version of GCC.
+        # Even if GCC is provided by default from macOS, we want to upgrade it if possible.
         # TODO: find a way to use gcc@9
         formulae+=(gcc)
     fi
@@ -11,7 +11,6 @@ function install_osx() {
     if [[ "$RUN_TESTS_WITH_COVERAGE" == "true" ]]; then
         # Install LCOV only when the tests are run with coverage.
         # This is optimal since we don't want to install it in jobs which don't use it.
-        # Without specifying any version, it will grab the latest one.
         formulae+=(lcov)
     fi
 
@@ -62,19 +61,22 @@ function install_osx() {
 function install_linux() {
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo add-apt-repository ppa:jonathonf/llvm -y
-    sudo add-apt-repository 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main'
+    sudo add-apt-repository 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main' -y
     sudo apt-get update
 
     if [[ "$CC" == "$CC_GCC_LINUX" ]]; then
         #
         sudo apt-get install g++-9
     elif [[ "$CC" == "$CC_CLANG" ]]; then
-#        wget https://apt.llvm.org/llvm.sh
-#        chmod +x llvm.sh
-#        sudo ./llvm.sh 9
-#        sudo apt-get install llvm-9-dev
-#        sudo ./llvm.sh 9
+        #        wget https://apt.llvm.org/llvm.sh
+        #        chmod +x llvm.sh
+        #        sudo ./llvm.sh 9
+        #        sudo apt-get install llvm-9-dev
+        #        sudo ./llvm.sh 9
         sudo apt-get install clang-9
+        which clang-9
+        clang-9 --version
+        dpkg -L clang-9
     fi
 
     # Install LCOV only when the tests are run with coverage.
