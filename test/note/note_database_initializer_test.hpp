ed9d96c5ba50d92fa12351f6d2e80ec1bef37537
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-10-19 13:51:38 +0200

    Fix inline variables on GCC 6

--- test/note/note_database_initializer_test.hpp
+++ test/note/note_database_initializer_test.hpp
@@ -4,7 +4,11 @@
 
 class NoteDatabaseInitializerTest : public ::testing::Test {
    protected:
+#if __cpp_inline_variables
     inline static const std::string testDbPath = "note_database_initializer_test.db";
+#else
+    static const std::string testDbPath;
+#endif
 
     static void changeVersion(int version);
 
